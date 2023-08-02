5f63493b767e671461677b91d17ae4663d4cd2a4
Gitanshk <gitanshkothari@gmail.com>
2023-08-02 19:48:23 -0400

    added first 3d orange plane and red box

--- script.js
+++ script.js
@@ -46,23 +46,67 @@
     renderer.setSize(mlCanvas.clientWidth, mlCanvas.clientHeight);
     mlCanvas.appendChild(renderer.domElement);
   
-    // Create a simple 3D neural network representation
-    const geometry = new THREE.BoxGeometry(10, 10, 10);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const neuralNetwork = new THREE.Mesh(geometry, material);
-    scene.add(neuralNetwork);
-  
+    // // Create a simple 3D neural network representation
+    // const geometry = new THREE.BoxGeometry(3, 3, 3);
+    // const material = new THREE.MeshBasicMaterial({ color: 0xd35400 });
+    // const neuralNetwork = new THREE.Mesh(geometry, material);
+    // scene.add(neuralNetwork);
+
+
+    const groundGeometry = new THREE.PlaneGeometry(100, 100);
+    const groundMaterial = new THREE.MeshPhongMaterial({ color: 0xd35400, side: THREE.DoubleSide });
+    const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+    // ground.rotation.x = -Math.PI / 2; // Rotate the ground to be horizontal
+    scene.add(ground);
+
+    // Add a car model (you can use any 3D model you like)
+    const carGeometry = new THREE.BoxGeometry(3, 1, 2);
+    const carMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+    const car = new THREE.Mesh(carGeometry, carMaterial);
+    car.position.set(0, 0.25, 0);
+    scene.add(car);
+
+
+      // Add lighting to the scene
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Ambient light to add overall illumination
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Directional light to simulate sunlight
+    directionalLight.position.set(1, 1, 1); // Set the light's direction
+    scene.add(ambientLight, directionalLight);
+
     camera.position.z = 5;
   
     // Animation loop
     function animate() {
       requestAnimationFrame(animate);
-      neuralNetwork.rotation.x += 0.01;
-      neuralNetwork.rotation.y += 0.01;
+      // neuralNetwork.rotation.x += 0.01;
+      // neuralNetwork.rotation.y += 0.01;
       renderer.render(scene, camera);
     }
   
     animate();
+
+
+    function updateCarPosition() {
+      document.addEventListener("keydown", (event) => {
+        const speed = 0.1;
+        switch (event.key) {
+          case "w":
+            car.position.z -= speed;
+            break;
+          case "a":
+            car.position.x -= speed;
+            break;
+          case "s":
+            car.position.z += speed;
+            break;
+          case "d":
+            car.position.x += speed;
+            break;
+        }
+      });
+    }
+  
+    updateCarPosition();
   }
   
   // Call the function to create the 3D machine learning visualization
